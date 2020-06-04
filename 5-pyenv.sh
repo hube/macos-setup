518faddc487e6f4b7e73d8ab96c728abb932a9d9
Hubert Lee <hube@users.noreply.github.com>
2020-06-04 09:24:57 -0700

    Update setup script

---
+++ 5-pyenv.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# From https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+brew install openssl readline sqlite3 xz zlib
--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ source 1-oh-my-zsh.sh
 source 2-homebrew.sh
 source 3-macos.sh
 source 4-git.sh
+source 5-pyenv.sh
+source 6-home.sh
+source 7-sublime-text.sh
