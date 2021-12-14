e555a71fb7fa5e32f02a93737071b43437591fa3
Hubert Lee 🤖 <hube@users.noreply.github.com>
2021-12-14 08:43:39 -0800

    Update SublimeText path

--- 7-sublime-text.sh
+++ 7-sublime-text.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cp sublime-text3/Preferences.sublime-settings \
-  "/Users/hube/Library/Application Support/Sublime Text 3/Packages/User"
+  "/Users/hube/Library/Application Support/Sublime Text/Packages/User"
