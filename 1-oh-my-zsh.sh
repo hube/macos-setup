f30e68f7d8e88ea24879e4140bce91a9d06c672b
Hubert Lee 🤖 <hube@users.noreply.github.com>
2021-12-14 21:20:08 -0800

    Add nvm configuration

--- 1-oh-my-zsh.sh
+++ 1-oh-my-zsh.sh
@@ -3,4 +3,4 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-cp oh-my-zsh/hube.zsh ~/.oh-my-zsh/custom/.
+cp oh-my-zsh/* ~/.oh-my-zsh/custom/.
