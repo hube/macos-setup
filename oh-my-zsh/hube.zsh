440be5f734c1cea5c7aa58847f13ba1edfa4d429
Hubert Lee 🤖 <hube@users.noreply.github.com>
2021-12-14 21:13:37 -0800

    Update zsh config

--- oh-my-zsh/hube.zsh
+++ oh-my-zsh/hube.zsh
@@ -1,8 +1,6 @@
+export EDITOR=vim
+
 alias buc='brew upgrade && brew cleanup'
-
-alias gcm='git commit -m'
-
-export PATH="/usr/local/opt/postgresql@11/bin:$PATH"
 
 # Unset LESS
 # https://github.com/ohmyzsh/ohmyzsh/pull/6611
@@ -11,4 +9,4 @@ unset LESS
 unsetopt share_history
 
 eval "$(pyenv init -)"
-eval "$(rbenv init -)"
+eval "$(rbenv init - zsh)"
