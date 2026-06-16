213f52bd9fa467660eead2d9760849e5e3b5f92c
Hubert Lee 🤖 <hube@users.noreply.github.com>
2026-06-16 12:42:02 -0700

    Simplify homebrew configuration

--- 2-homebrew.sh
+++ 2-homebrew.sh
@@ -6,7 +6,31 @@
 # Inspect system status
 brew doctor
 
-source homebrew/brew.sh
+brew install \
+  git \
+  libusb \
+  vim \
+  zsh
+
+# Casks
+brew install \
+  1password \
+  adobe-acrobat-reader \
+  docker-desktop \
+  firefox \
+  gnucash \
+  google-chrome \
+  heroic \
+  iterm2 \
+  karabiner-elements \
+  obsidian \
+  rectangle \
+  scroll-reverser \
+  steam \
+  sublime-text \
+  the-unarchiver \
+  vlc \
+  --cask
 
 # Remove outdated versions from the cellar
 brew cleanup
