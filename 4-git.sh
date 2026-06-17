1f73a0d9ccf757dd515887904381c948afeb0161
Hubert Lee 🤖 <395901+hube@users.noreply.github.com>
2026-06-16 17:29:20 -0700

    Set user.signingKey git config

--- 4-git.sh
+++ 4-git.sh
@@ -7,8 +7,20 @@ GIT_USER_EMAIL=395901+hube@users.noreply.github.com
 SSH_PRIVATE_KEY=~/.ssh/id_ed25519
 SSH_PUBLIC_KEY=${SSH_PRIVATE_KEY}.pub
 
+# Create an SSH key with a comment like "hube@Twoflower"
+source 4.1-setup-ssh-key.sh $SSH_PRIVATE_KEY "$(id -un)@$(hostname)"
+
+# Add an entry with the git user email to the SSH allowed_signers file
+# Allows for the signature to be recognized when running
+# `git log --show-signature`
+echo -n "${GIT_USER_EMAIL} " >> $ALLOWED_SIGNERS_PATH
+cat $SSH_PUBLIC_KEY >> $ALLOWED_SIGNERS_PATH
+
+# Setup git
+
 git config set --global user.name "Hubert Lee 🤖"
 git config set --global user.email $GIT_USER_EMAIL
+git config set --global user.signingKey $SSH_PUBLIC_KEY
 
 git config set --global init.defaultBranch main
 git config set --global core.excludesfile ~/.gitignore
@@ -18,12 +30,3 @@ git config set --global commit.gpgsign true
 git config set --global tag.gpgsign true
 git config set --global gpg.format ssh
 git config set --global gpg.ssh.allowedSignersFile $ALLOWED_SIGNERS_PATH
-
-# Create an SSH key with a comment like "hube@Twoflower"
-source 4.1-setup-ssh-key.sh $SSH_PRIVATE_KEY "$(id -un)@$(hostname)"
-
-# Add an entry with the git user email to the SSH allowed_signers file
-# Allows for the signature to be recognized when running
-# `git log --show-signature`
-echo -n "${GIT_USER_EMAIL} " >> $ALLOWED_SIGNERS_PATH
-cat $SSH_PUBLIC_KEY >> $ALLOWED_SIGNERS_PATH
